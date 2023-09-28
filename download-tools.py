74ef8af36ef69d4212f9785ba2b65ca3b0f6ed0c
kareem77g7 <129593536+kareem77g7@users.noreply.github.com>
2023-09-28 11:21:37 +0300

    Add files via upload

---
+++ download-tools.py
@@ -0,0 +1,247 @@
+import os
+import sys
+import time
+from logo import *
+os.system ("\033[40m")
+os.system ("clear")
+os.system ("pkg install toilet")
+os.system ("clear")
+def kareem (Z):
+    for e in Z +'\n':
+        sys.stdout.write(e)
+        sys.stdout.flush ()
+        time.sleep (00000.03)
+kareem ("\033[1;36mThis tool is made by developer\033[1;33m Kareem Abd Alziz.\033[1;36m This tool is for educational purposes and not for anything else.")
+def jalan (Z):
+    for e in Z +'\n':
+        sys.stdout.write(e)
+        sys.stdout.flush ()
+        time.sleep (00000.02)
+print ("")
+logo()
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+#############################################
+
+B="\033[1;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+B="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # White
+BB="\033[40m"  # Back Ground Black
+##########№###################################
+
+
+termux = 1
+hedra = 2
+metasploitframework = 3
+sqlmap = 4
+AdminHack = 5
+wifite = 6
+ipdrone = 7
+Amer = 8
+XerXes = 9
+inshackle = 10
+evil_droid = 11
+Ubuntu = 12
+Fedora = 13
+Kali = 14
+Kali_Nethunter = 15
+Parrot = 16
+BackBox = 17
+Black_Arch = 18
+Header = 19
+TermuxBanner = 20
+ExploitAndroidStagefright = 21
+wafw00f = 22
+DarkFlyTool = 23
+Nmap = 24
+PhoneSploit = 25
+Report = 26
+TXTOOL = 27
+Lazymux = 28
+hammer = 29
+ARat = 30
+EasYHaCk = 31
+
+#############################################
+
+jalan (BB+R+"1 »»» start_termux")
+print ("")
+jalan (BB+"2 »»» download_hedra")
+print ("")
+jalan (BB+"3 »»» download_metasploit-framework")
+print ("")
+jalan (BB+"4 »»» download_sqlmap")
+print ("")
+jalan ("5 »»» download_AdminHack")
+print ("")
+jalan ("6 »»» download_wifite")
+print ("")
+jalan ("7 »»» download_ipdrone")
+print ("")
+jalan ("8 »»» download_Amer")
+print ("")
+jalan ("9 »»» download_XerXes")
+print ("")
+jalan ("10 »»» download_inshackle")
+print ("")
+jalan ("11 »»» download_evil droid")
+print ("")
+jalan ("12 »»» download_Ubuntu")
+print ("")
+jalan ("13 »»» download_Fedora")
+print ("")
+jalan ("14 »»» download_Kali")
+print ("")
+jalan ("15 »»» download_Kali_Nethunter")
+print ("")
+jalan ("16 »»» download_Parrot")
+print ("")
+jalan ("17 »»» download_BackBox")
+print ("")
+jalan ("18 »»» download_Black_Arch")
+print ("")
+jalan ("19 »»» download_T-Header/")
+print ("")
+jalan ("20 »»» download_Termux-Banner")
+print ("")
+jalan ("21 »»» download_Exploit-Android-Stagefright")
+print ("")
+jalan ("22 »»» download_wafw00f")
+print ("")
+jalan ("23 »»» download_DarkFly-Tool")
+print ("")
+jalan ("24 »»» download_Nmap")
+print ("")
+jalan ("25 »»» download_PhoneSploit")
+print ("")
+jalan ("26 »»» download_Report")
+print ("")
+jalan ("27 »»» download_TXTOOL")
+print ("")
+jalan ("28 »»» download_Lazymux")
+print ("")
+jalan ("29 »»» download_hammer")
+print ("")
+jalan ("30 »»» download_A-Rat")
+print ("")
+jalan ("31 »»» download_EasY_HaCk")
+print ("")
+jalan ("99 »»» Update the Download-Termux-Tools")
+print ("")
+
+
+#################
+print ("")
+print ("")
+print ("")
+print ("")
+number = input (BB+Y+"Type the tool number\033[1;35m >>>>>> "+G) 
+if number == "1" :
+    os.system ("")
+    os.system ("python download-tools.py")
+elif number == "2" :
+    os.system ("")
+    os.system ("python download-tools.py")
+elif number == "3" :
+    os.system ("")
+    os.system ("python download-tools.py")
+elif number == "4" :
+    os.system ("git clone https://github.com/sqlmapproject/sqlmap.git")
+    os.system ("python download-tools.py")
+elif number == "5" :
+    os.system ("git clone https://github.com/mishakorzik/AdminHack")
+    os.system ("python download-tools.py")
+elif number == "6" :
+    os.system ("git clone https://github.com/derv82/wifite.git")
+    os.system ("python download-tools.py")
+elif number == "7" :
+    os.system ("git clone https://github.com/noob-hackers/ipdrone")
+    os.system ("python download-tools.py")
+elif number == "8" :
+    os.system ("git clone https://github.com/Amerlaceset/Amer")
+    os.system ("python download-tools.py")
+elif number == "9" :
+    os.system ("git clone https://github.com/CyberXCodder/XerXes.git")
+    os.system ("python download-tools.py")
+elif number == "10" :
+    os.system ("git clone https://github.com/thelinuxchoice/inshackle.git")
+    os.system ("python download-tools.py")
+elif number == "11" :
+    os.system ("git clone https://github.com/M4sc3r4n0/Evil- Droid.git")
+    os.system ("python download-tools.py")
+elif number == "12" :
+    os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh")
+    os.system ("python download-tools.py")
+elif number == "13" :
+    os.system ("pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Fedora/fedora.sh && bash fedora.sh")
+    os.system ("python download-tools.py")
+elif number == "14" :
+    os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh")
+    os.system ("python download-tools.py")
+elif number == "15" :
+    os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Nethunter/nethunter.sh && bash nethunter.sh")
+    os.system ("python download-tools.py")
+elif number == "16" :
+    os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh")
+    os.system ("python download-tools.py")
+elif number == "17" :
+    os.system ("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/BackBox/backbox.sh && bash backbox.sh")
+    os.system ("python download-tools.py")
+elif number == "18" :
+    os.system ("pacman-key --init && pacman-key --populate archlinuxarm && pacman -Sy --noconfirm curl && curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && ./strap.sh")
+    os.system ("python download-tools.py")
+elif number == "19" :
+    os.system ("git clone https://github.com/remo7777/T-Header.git")
+
+elif number == "20" :
+    os.system ("git clone https://github.com/Bhai4You/Termux-Banner")
+    os.system ("python download-tools.py")
+elif number == "21" :
+    os.system ("git clone https://github.com/fireworm0/Exploit-Android-Stagefright")
+    os.system ("python download-tools.py")
+elif number == "22" :
+    os.system ("pip install wafw00f")
+    os.system ("python download-tools.py")
+elif number == "23" :
+    os.system ("git clone https://github.com/Ranginang67/DarkFly-Tool ")
+    os.system ("python download-tools.py")
+elif number == "24" :
+    os.system ("pkg install Nmap")
+    os.system ("python download-tools.py")
+elif number == "25" :
+    os.system ("git clone https://github.com/Zucccs/PhoneSploit")
+    os.system ("python download-tools.py")
+elif number == "26" :
+    os.system ("git clone https://github.com/IlayTamvan/Report")
+    os.system ("python download-tools.py")
+elif number == "27" :
+    os.system ("git clone https://github.com/kuburan/txtool.git")
+    os.system ("python download-tools.py")
+elif number == "28" :
+    os.system ("git clone https://github.com/Gameye98/Lazymux")
+    os.system ("python download-tools.py")
+elif number == "29" :
+    os.system ("git clone https://github.com/cyweb/hammer")
+    os.system ("python download-tools.py")
+elif number == "30" :
+    os.system ("git clone https://github.com/Xi4u7/A-Rat")
+    os.system ("python download-tools.py")
+elif number == "31" :
+    os.system ("git clone https://github.com/sabri-zaki/EasY_HaCk")
+    os.system ("python download-tools.py")
+elif number == "99" :
+    os.system ("git pull")
+    os.system ("python download-tools.py")
+else:
+    print("erre.....!")
+    os.system ("python download-tools.py")
